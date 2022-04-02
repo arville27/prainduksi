2efcfab1ca5367bc12df5677321a5b5da61f39e9
Daniel Wijaya <dwjy27@gmail.com>
2022-04-02 20:02:55 +0700

    Add missing element on toggle blur between transition

--- src/static/js/index.js
+++ src/static/js/index.js
@@ -46,7 +46,7 @@ $(document).on('submit', '#password-form', async (e) => {
     const res = await fetch('/api/auth', option);
     if (res.status === 200) {
         const container = $('.c-3');
-        $('.content').toggleClass('blur');
+        $('.content, .bg-vid').toggleClass('blur');
         await $('.popup-container').fadeOut(350).promise();
 
         $('.c-1, .sosmed-container').css('display', 'none');
