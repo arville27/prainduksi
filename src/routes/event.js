c024c8ec51a7a76d42515ec9647dd25a4805076b
Daniel Wijaya <dwjy27@gmail.com>
2022-04-03 23:45:08 +0700

    Finalize data.json structure

--- src/routes/event.js
+++ src/routes/event.js
@@ -1,21 +1,33 @@
 const express = require('express');
 const route = express.Router();
 const { organizer, participants } = require('../config');
+const authorization = require('../middlerware/authorization');
 
-route.get('/members', (_, res) => res.json({ organizer, participants }));
+const participantsWithoutLink = participants.map((participant) => {
+    delete participant['link'];
+    return participant;
+});
 
-route.get('/participant', (_, res) => res.json(participants));
+route.get('/members', (_, res) => res.json({ organizer, participants: participantsWithoutLink }));
+
+route.get('/participant', (_, res) => res.json(participantsWithoutLink));
 
 route.get('/organizer', (_, res) => res.json(organizer));
 
 route.get('/participant/:id', (req, res) => {
-    const participant = participants.find((group) => group.id === req.params.id);
+    const participant = participantsWithoutLink.find((group) => group.id === req.params.id);
     if (participant) res.json(participant);
     else res.sendStatus(404);
 });
 
-route.get('/active', (_, res) => {
+route.get('/active/link', authorization, (req, res) => {
+    if (!req.valid) return res.sendStatus(401);
     const active = participants.find((group) => group.active === 1);
+    res.json({ link: active.link });
+});
+
+route.get('/active', (_, res) => {
+    const active = participantsWithoutLink.find((group) => group.active === 1);
     res.json(active);
 });
 
