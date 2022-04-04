dd608caa134a684253ad9c2da9a97ed75b9f2d25
Daniel Wijaya <dwjy27@gmail.com>
2022-04-04 21:23:03 +0700

    Add command to toggle notification

---
+++ src/commands/toggleStates.js
@@ -0,0 +1,33 @@
+const { GROUP_ID, data } = require('../config');
+const { saveData } = require('../utility');
+
+module.exports = {
+    name: 'Toggle db states',
+    matcher: function (client, event) {
+        return (
+            event.message.text.match(/^x toggle /i) !== null &&
+            event.source.type === 'group' &&
+            event.source.groupId === GROUP_ID
+        );
+    },
+    run: async (client, event) => {
+        const arg = event.message.text.slice(9).trim();
+        let messages;
+        if (Object.hasOwnProperty.call(data.states, arg)) {
+            if (arg === 'admin notif') {
+                data.states.admin_notification = !data.states.admin_notification;
+                messages = `Admin auth notification is set to ${data.states.admin_notification}`;
+            } else if (arg === 'notif') {
+                data.states.notification = !data.states.notification;
+                messages = `Kelas auth notification is set to ${data.states.notification}`;
+            }
+            await saveData(data);
+        } else {
+            messages = 'Failed to toggle a state, please check if the key is exists';
+        }
+        return client.replyMessage(event.replyToken, {
+            type: 'text',
+            text: messages,
+        });
+    },
+};
--- src/middlerware/pushNotification.js
+++ src/middlerware/pushNotification.js
@@ -1,11 +1,11 @@
-const { SECRET, ADMIN_SECRET, data, GROUP_ID } = require('../config');
+const { SECRET, ADMIN_SECRET, data, GROUP_ID, states } = require('../config');
 const client = require('../app');
 const { saveData } = require('../utility');
 
 module.exports = async (req, _, next) => {
     next();
     data.states.trial += 1;
-    if (req.body.secret === SECRET && !data.states.guessed) {
+    if (states.notification && req.body.secret === SECRET && !data.states.guessed) {
         data.states.guessed = true;
         const active = data.participants.find((group) => group.active === 1);
         if (GROUP_ID) {
@@ -16,7 +16,7 @@ module.exports = async (req, _, next) => {
                 })
                 .catch(console.log);
         }
-    } else if (req.body.secret === ADMIN_SECRET) {
+    } else if (states.admin_notification && req.body.secret === ADMIN_SECRET) {
         data.states.trial -= 1;
         if (GROUP_ID) {
             client
